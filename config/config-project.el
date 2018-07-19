a5a6e0d70415ca510139051fd1277dde72902447
Chedal-Anglay Julien <chedala.julien@gmail.com>
2018-07-19 16:34:04 +0200

    Updated.

--- config/config-project.el
+++ config/config-project.el
@@ -8,6 +8,18 @@
 
 ;; Magit settings
 (require 'evil-magit)
+(setq magit-refresh-status-buffer nil)
+(setq magit-diff-highlight-indentation nil)
+(setq magit-diff-highlight-trailing nil)
+(setq magit-diff-paint-whitespace nil)
+(setq magit-diff-highlight-hunk-body nil)
+(setq magit-diff-refine-hunk nil)
+(setq magit-revision-insert-related-refs nil)
+(remove-hook 'server-switch-hook 'magit-commit-diff)
+(remove-hook 'magit-refs-sections-hook 'magit-insert-tags)
+(setq vc-handled-backends nil)
+
+
 (evil-magit-init)
 
 
