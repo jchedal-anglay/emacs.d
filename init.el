148e92aad02d37a5e07c8a2b41216aa5ac1e06c1
Julien Chedal-Anglay <chedala.julien@gmail.com>
2019-07-22 01:49:31 +0200

    Wrapped init file to avoid unnecessary filename parsing

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
 ;; Required so it isn't automatically added by package.el
 ;; (package-initialize)
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(let ((file-name-handler-alist nil))
+  (org-babel-load-file (concat user-emacs-directory "config.org")))
