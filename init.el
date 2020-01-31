4a55839ce26feea51a743b9f2bce07a3692ff256
Julien CHEDAL-ANGLAY <jchedal-@e3r7p12.42.fr>
2020-01-31 18:27:36 +0100

    init: removed useless comment

--- init.el
+++ init.el
@@ -1,4 +1,2 @@
-;; Required so it isn't automatically added by package.el
-;; (package-initialize)
 (let ((file-name-handler-alist nil))
   (org-babel-load-file (concat user-emacs-directory "config.org")))
