e61ccbd1023305230bd24e39eb1decffea272775
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-03-02 00:38:40 +0100

    core: Added lexical binding to init.el

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t -*-
+
 (defconst igneous/required-version "25.1")
 (when (version< emacs-version igneous/required-version)
   (error "Requires GNU Emacs %s or newer, but current version is %s" igneous/required-version emacs-version))
