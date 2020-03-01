1ceda25dc8178a463b80d050a65d294891c7fe3d
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-03-02 00:20:41 +0100

    core: Disable GC during start-up and set to more sensible values

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
 (defconst igneous/required-version "25.1")
 (when (version< emacs-version igneous/required-version)
   (error "Requires GNU Emacs %s or newer, but current version is %s" igneous/required-version emacs-version))
+
+(setq gc-cons-threshold most-positive-fixnum)
+(setq gc-cons-percentage 0.6)
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold (* 32 1000000)
+                                            gc-cons-percentage 0.1)))
