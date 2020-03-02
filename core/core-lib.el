e6b74ab5a65af8165489ef4a9834548029508a4b
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-03-02 03:23:23 +0100

    core: Added core-lib module

---
+++ core/core-lib.el
@@ -0,0 +1,7 @@
+;;; core-lib.el --- Core module that defines utility functions and macros -*- lexical-binding: t -*-
+;;; Commentary:
+;; Defines functions and macros used throughout the configuration
+;;; Code:
+
+(provide 'core-lib)
+;;; core-lib.el ends here
