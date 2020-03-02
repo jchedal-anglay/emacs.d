7455d95da0a6374647a0e03e176c6d886d8223e5
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-03-02 02:59:43 +0100

    core: Added core-custom module

---
+++ core/core-custom.el
@@ -0,0 +1,7 @@
+;;; core-custom.el --- Core module that initializes variables -*- lexical-binding: t -*-
+;;; Commentary:
+;; Defines variables and constants used throughout the configuration
+;;; Code:
+
+(provide 'core-custom)
+;;; core-custom.el ends here
--- core/core.el
+++ core/core.el
@@ -3,5 +3,11 @@
 ;; Loads all other core modules.
 ;;; Code:
 
+(defvar igneous//core-modules
+  '(core-custom))
+
+(dolist (core-module igneous//core-modules)
+  (require core-module))
+
 (provide 'core)
 ;;; core.el ends here
