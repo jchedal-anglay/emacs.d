f73bb555fea90605d8e94605f791c788dbddd893
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-03-14 14:34:30 +0100

    core: Added workaround for (package-initialize)

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
+(setq package--init-file-ensured t)
+
 (defconst igneous-required-version "25.1")
 (when (version< emacs-version igneous-required-version)
   (error "Requires GNU Emacs %s or newer, but current version is %s" igneous-required-version emacs-version))
