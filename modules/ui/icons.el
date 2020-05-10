f9e3ee59a9e74dacce8c2d3fe20017749d3422b5
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-05-10 05:53:35 +0200

    ui/icons: Added icons in startup dashboard

--- modules/ui/icons.el
+++ modules/ui/icons.el
@@ -3,3 +3,7 @@
   :config
   (unless (find-font (font-spec :name "all-the-icons"))
     (all-the-icons-install-fonts t)))
+
+(with-eval-after-load 'dashboard
+  (setq dashboard-set-file-icons t
+        dashboard-set-heading-icons t))
