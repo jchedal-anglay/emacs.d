1afce604361bb30c3e1ecf62f41b5ca4e2f4ac7b
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-05-15 03:54:30 +0200

    tools/git: Added git support via magit

---
+++ modules/tools/git.el
@@ -0,0 +1,22 @@
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-fullframe-status-topleft-v1)
+  :bind
+  ("C-c g" . magit)
+  (:map magit-status-mode-map
+        ("q" . (lambda () (interactive) (magit-mode-bury-buffer 16)))))
+
+(use-package vc-hooks
+  :ensure nil
+  :after magit
+  :custom
+  (vc-handled-backends nil))
+
+(use-package transient
+  :after magit
+  :config
+  (transient-bind-q-to-quit))
+
+(use-package gitattributes-mode)
+(use-package gitignore-mode)
+(use-package gitconfig-mode)
