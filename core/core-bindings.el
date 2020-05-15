cfff717623c72038f0b199d20f94ebef2c721c4f
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-05-15 04:07:18 +0200

    core: Added binding to clear screen in comint-mode

--- core/core-bindings.el
+++ core/core-bindings.el
@@ -5,6 +5,7 @@
 
 (global-set-key (kbd "C-x C-b") #'ibuffer)
 (define-key 'dired-mode-map (kbd "C-c C-c") #'wdired-change-to-wdired-mode)
+(define-key 'comint-mode-map (kbd "C-l") #'comint-clear-buffer)
 
 (use-package mwim
   :bind
