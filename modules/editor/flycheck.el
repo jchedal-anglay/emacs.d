b9d96be2e968e9832dd21d05f23da6e417fedbcc
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-05-21 06:48:03 +0200

    editor/flycheck: Added basic flycheck configuration

---
+++ modules/editor/flycheck.el
@@ -0,0 +1,30 @@
+(use-package flycheck
+  :hook
+  (prog-mode . flycheck-mode)
+  :custom-face
+  (flycheck-info ((t (:underline (:style line :color "#80FF80")))))
+  (flycheck-warning ((t (:underline (:style line :color "#FF9933")))))
+  (flycheck-error ((t (:underline (:style line :color "#FF5C33")))))
+  (flycheck-check-syntax-automatically '(mode-enabled save))
+  :custom
+  (flycheck-checkers nil)
+  :config
+  ;; Bad flycheck design, override the default fringe without having to redefine entire checker levels
+  (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
+    [#b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111
+     #b00000111]))
