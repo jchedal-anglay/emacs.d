a3ea243fad44bf7851e4386eaa98878daf8fa09c
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-05-21 06:47:42 +0200

    editor/company: Added basic company configuration

---
+++ modules/editor/company.el
@@ -0,0 +1,17 @@
+(use-package company
+  :bind
+  ("M-/" . company-complete)
+  (:map company-active-map
+	("M-/" . company-other-backend)
+	("M-n" . nil)
+	("M-p" . nil)
+	("C-n" . company-select-next)
+	("C-p" . company-select-previous))
+  :custom
+  (company-require-match 'never)
+  (company-dabbrev-downcase nil)
+  (company-tooltip-align-annotations t)
+  (company-idle-delay nil)
+  (company-backends nil)
+  :config
+  (global-company-mode t))
