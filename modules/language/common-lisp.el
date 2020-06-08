e2362d2ba51fbf82157602298b20fd57875ef88b
Julien Chedal-Anglay <chedala.julien@gmail.com>
2020-06-09 00:48:51 +0200

    language/common-lisp: Removed overlays, need fix later

--- modules/language/common-lisp.el
+++ modules/language/common-lisp.el
@@ -9,6 +9,7 @@
   (sly-kill-without-query-p t)
   (sly-net-coding-system 'utf-8-unix)
   (sly-complete-symbol-function 'sly-simple-completions)
+  (sly-mrepl-pop-sylvester nil)
   :config
   (sly-setup)
   ;; Hack to override the :clear-repl-history listener
@@ -20,52 +21,6 @@
 (with-module! :editor company
   (add-hook 'sly-mode-hook #'company-mode))
 
-;; Code taken from https://endlessparentheses.com/eval-result-overlays-in-emacs-lisp.html
-;; Adapted for SLY
-(with-module! :language clojure
-  (autoload 'cider--make-result-overlay "cider-overlays")
-
-  (defun endless/eval-overlay-cl (value point)
-    (cider--make-result-overlay (format "%s" value)
-      :where point
-      :duration 'command)
-    value)
-
-  (advice-add 'sly-eval-with-transcript :override
-              (lambda (form)
-                "Eval FORM in Lisp. Display output, if any."
-                (run-hooks 'sly-transcript-start-hook)
-                (sly-rex () (form)
-                  ((:ok value)
-                   (setq igneous--sly-eval-result value)
-                   (run-hooks 'sly-transcript-stop-hook)
-                   (sly-display-eval-result value))
-                  ((:abort condition)
-                   (run-hooks 'sly-transcript-stop-hook)
-                   (sly-message "Evaluation aborted on %s." condition)))))
-
-  (advice-add 'sly-eval-region :around
-              (lambda (f beg end &rest _)
-                (prog1
-                    (endless/eval-overlay-cl
-                     (apply f beg end igneous--sly-eval-result)
-                     end)
-                  (makunbound 'igneous--sly-eval-result))))
-
-  (advice-add 'sly-eval-last-expression :filter-return
-              (lambda (_)
-                (prog1
-                    (endless/eval-overlay-cl igneous--sly-eval-result (point))
-                  (makunbound 'igneous--sly-eval-result))))
-
-  (advice-add 'sly-eval-defun :filter-return
-              (lambda (_)
-                (prog1
-                    (endless/eval-overlay-cl
-                     igneous--sly-eval-result
-                     (save-excursion
-                       (end-of-defun)
-                       (point)))
-                  (makunbound 'igneous--sly-eval-result)))))
+;; TODO: Add overlays, method used for elisp doesn't work
 
 (dependencies! sbcl)
